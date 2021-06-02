69db778eb900f6a7b7bc03dad5578483b2efc5e6
Emily <emilynadler12@gmail.com>
2021-06-02 13:12:30 -0400

    Practicing with hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
+
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", { id: "my-brand" }, "adopt me!"),
+//     React.createElement(Pet, {
+//       name: "Luna",
+//       animal: "Dog",
+//       breed: "Havanese",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Pepper",
+//       animal: "Bird",
+//       breed: "Cockatiel",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Sudo",
+//       animal: "Dog",
+//       breed: "Wheaton Terrior",
+//     }),
+//   ]);
+// };
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-brand" }, "adopt me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Sudo",
-      animal: "Dog",
-      breed: "Wheaton Terrior",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/SearchParams.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { useState } from "react";
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
+
+  function updateLocation(e) {
+    setLocation(e.target.value);
+  }
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="Location">
+          Location
+          <input
+            id="location"
+            onChange={updateLocation}
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+        {/* Animal label */}
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option value=""></option>
+            {ANIMALS.map((animal) => {
+              return (
+                <option value={animal} key={animal}>
+                  {animal}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+        {/* Breed label */}
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option value=""></option>
+            {breeds.map((breed) => {
+              return (
+                <option value={breed} key={breed}>
+                  {breed}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
